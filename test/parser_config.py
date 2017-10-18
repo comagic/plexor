b1cf74affc28914a79ec58043fcd1e29476c092a
Dima Beloborodov <d.beloborodov@uiscom.ru>
2017-10-18 16:59:03 +0300

    test/parser_config.py has been added

---
+++ test/parser_config.py
@@ -0,0 +1,63 @@
+{
+    'init': [
+        {
+            'sql': 'create database parser_tests',
+            'dsn': 'dbname=postgres',
+        },
+        {
+            'sql': 'create extension if not exists plexor with schema pg_catalog',
+            'dsn': 'dbname=parser_tests',
+        },
+    ],
+    'final': [
+        {
+            'sql': 'drop database parser_tests',
+            'dsn': 'dbname=postgres',
+        },
+    ],
+    'query_print_format': '{query}\n{pgerror}',
+    'cycle_print_format': 'cycle {cycle:>2}: {check_stat}s',
+    'is_single_connect': True,
+    'is_init': False,
+    'is_test': True,
+    'is_final': False,
+    'dsn': 'dbname=parser_tests',
+    'tests': [
+        {
+            'query':
+            '\n'.join(
+                (
+                    'create or replace function cluster_keyword_error(anode_id integer)',
+                    'returns text',
+                    'language plexor as $$',
+                    'luster proxy;',
+                    'run on get_node(anode_id);',
+                    '$$;',
+                )
+            ),
+            'pgerror':
+            (
+                "ERROR:  Plexor function public.cluster_keyword_error(): "
+                "'cluster' keyword not found"
+            )
+
+        },
+        {
+            'query':
+            '\n'.join(
+                (
+                    'create or replace function syntax_error(anode_id integer)',
+                    'returns text',
+                    '    language plexor',
+                    '    as $$',
+                    '    cluster |proxy',
+                    '$$;',
+                )
+            ),
+            'pgerror':
+            (
+                "ERROR:  Plexor function public.syntax_error(): unexpected symbol '|'"
+            )
+        }
+    ]
+}
